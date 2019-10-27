c576346d21327f27bd16b0c9516fd6cba5ee9836
Tilen52 <tilen6452@gmail.com>
2019-10-27 20:29:23 +0300

    trying out new stuff

---
+++ lesson3_step8.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+browser = webdriver.Chrome()
+browser.get('http://suninjuly.github.io/explicit_wait2.html')
+
+button = WebDriverWait(browser, 20).until(
+    EC.text_to_be_present_in_element((By.ID, "price"), '$100')
+)
+browser.find_element_by_css_selector('#book').click()
+
+x = browser.find_element_by_css_selector('#input_value').text
+browser.find_element_by_css_selector('#answer').send_keys(calc(x))
+submit = browser.find_element_by_css_selector('#solve')
+#browser.execute_script("return arguments[0].scrollIntoView(true);", submit)
+submit.click()
